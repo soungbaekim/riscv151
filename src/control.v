a61e93be018322155e653dd6a6f9079742342ebc
Soung Bae Kim <sbkim@berkeley.edu>
2020-04-05 18:01:00 -0700

    commented willbranch check in always block

--- src/control.v
+++ src/control.v
@@ -192,7 +192,7 @@ module control(
       PC_Sel = `PCSEL_PLUS4;
     end
     */
-
+    /*
     if (will_branch == 1'b1) begin
 
       //PC_Sel = `PCSEL_ALU;
@@ -208,8 +208,9 @@ module control(
 
 
     end
-    else begin
 
+    else begin
+    */
       case (opcode)
       `OPC_LUI: begin
 
@@ -388,7 +389,7 @@ module control(
 
       endcase
 
-    end // else
+    //end // else
   end // always block
 
 endmodule
